173c62ebc92542d27651c9430dee5fef70dfcf9b
Peter Lewyckyj <lewyckyjp@gmail.com>
2022-04-03 22:06:32 -0400

    add very basic styling

--- src/routes/races.tsx
+++ src/routes/races.tsx
@@ -23,27 +23,28 @@ export default function Races() {
     }
     {
       !isLoading &&
-      <table style={{color: "black"}}>
-        <thead>
+      <table cellSpacing="0" cellPadding="0" style={{color: "black", margin: "0 auto"}}>
+        <thead style={{backgroundColor: "#98A5FF", color: "white", borderSpacing: 0}}>
           <tr>
-            <th style={{padding: "0 0.5rem"}}>Circuit</th>
-            <th style={{padding: "0 0.5rem"}}>Vehicle</th>
-            <th style={{padding: "0 0.5rem"}}>Laps</th>
-            <th style={{padding: "0 0.5rem"}}>Opponents</th>
-            <th style={{padding: "0 0.5rem"}}>Finish position</th>
-            <th style={{padding: "0 0.5rem"}}>Average lap time</th>
+            <th style={{padding: "0.5rem"}}>Circuit</th>
+            <th style={{padding: "0.5rem"}}>Vehicle</th>
+            <th style={{padding: "0.5rem"}}>Laps</th>
+            <th style={{padding: "0.5rem"}}>Opponents</th>
+            <th style={{padding: "0.5rem"}}>Finish position</th>
+            <th style={{padding: "0.5rem"}}>Average lap time</th>
+            <th style={{padding: "0.5rem"}}></th>
           </tr>
         </thead>
-        <tbody>
+        <tbody style={{backgroundColor: "#D1D7FF"}}>
           { races.map(race => (
           <tr key={race.id}>
-            <td style={{padding: "0 0.5rem"}}>{race.track_name}</td>
-            <td style={{padding: "0 0.5rem"}}>{race.vehicle}</td>
-            <td style={{padding: "0 0.5rem"}}>{race.laps}</td>
-            <td style={{padding: "0 0.5rem"}}>{race.num_opponents}</td>
-            <td style={{padding: "0 0.5rem"}}>{race.finish_pos || "error"}</td>
-            <td style={{padding: "0 0.5rem"}}>{race.avg_lap_time}s</td>
-            <td className="underline" style={{padding: "0 0.5rem"}}><a href={"/races/" + race.id}>details</a></td>
+            <td style={{padding: "0.5rem"}}>{race.track_name}</td>
+            <td style={{padding: "0.5rem"}}>{race.vehicle}</td>
+            <td style={{padding: "0.5rem"}}>{race.laps}</td>
+            <td style={{padding: "0.5rem"}}>{race.num_opponents}</td>
+            <td style={{padding: "0.5rem"}}>{race.finish_pos || "error"}</td>
+            <td style={{padding: "0.5rem"}}>{race.avg_lap_time}s</td>
+            <td className="underline" style={{padding: "0.5rem"}}><a href={"/races/" + race.id}>details</a></td>
           </tr>
           ))}
         </tbody>
